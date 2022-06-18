841f88e4d3b14481136573311fef8deb0492bb6d
Sivan Itescu <sivan.avital@gmail.com>
2022-06-19 01:37:09 +0300

    Adding Job and Jobs Resources initial implementation

--- RecruitEmployeeSystem/app.py
+++ RecruitEmployeeSystem/app.py
@@ -1,26 +1,46 @@
 import json
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
 from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
+api = Api(app)
 
-app.config['MONGODB_SETTINGS'] = {
-    'db': 'recruiter_database',
-    'host': 'localhost',
-    'port': 27017
-}
+# #Data base initialization
+# app.config['MONGODB_SETTINGS'] = {
+#     'db': 'recruiter_database',
+#     'host': 'localhost',
+#     'port': 27017
+# }
+#
+# db = MongoEngine()
+# db.init_app(app)
+#
+# class User(db.Document):
+#     name = db.StringField()
+#     email = db.StringField()
 
-db = MongoEngine()
-db.init_app(app)
-
-class User(db.Document):
-    name = db.StringField()
-    email = db.StringField()
+jobs = {}
 
 
-@app.route('/')
+class Jobs(Resource):
+    def get(self):
+        return jobs
+
+class Job(Resource):
+    def get(self, job_id):
+        return {job_id: jobs[job_id]}
+
+    def post(self,job_id):
+        jobs[job_id] = request.form['job_title']
+        return {job_id: jobs[job_id]}, 201
+
+
+api.add_resource(Jobs, '/alljobs')
+api.add_resource(Job, '/jobs/<string:job_id>')
+
+@app.route('/about')
 def index():
-    print("Hello world should be here")
     return jsonify({'name': 'irit',
                     'email': 'find_a_job@hotmail.com'})
 
---
+++ RecruitEmployeeSystem/client_test.py
@@ -0,0 +1,26 @@
+import requests
+
+BASE = "http://127.0.0.1:5000/"
+
+# response = requests.get(BASE)
+# print(response.json())
+
+response = requests.post(BASE+'/jobs/1', data={'job_title': 'Senior SW Engineer at Goggle'})
+print(response.json())
+
+response = requests.post(BASE+'/jobs/2', data={'job_title': 'Junior SW Engineer at Goggle'})
+print(response.json())
+
+response = requests.post(BASE+'/jobs/3', data={'job_title': 'Senior SW Engineer at Amazon'})
+print(response.json())
+
+response = requests.post(BASE+'/jobs/4', data={'job_title': 'Senior SW Engineer at Apple'})
+print(response.json())
+
+# response = requests.get(BASE+'/jobs/1')
+# print(response.json())
+response = requests.get(BASE+'/alljobs')
+print(response.json())
+
+response = requests.get(BASE+'/about')
+print(response.json())
