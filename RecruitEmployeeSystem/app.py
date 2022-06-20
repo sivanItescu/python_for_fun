38a19e5cd9dd8184f46220b29a3f7dc3645aa059
Sivan Itescu <sivan.avital@gmail.com>
2022-06-20 14:00:17 +0300

    Using MongoEngine to save jobs - start

--- RecruitEmployeeSystem/app.py
+++ RecruitEmployeeSystem/app.py
@@ -2,42 +2,87 @@ import json
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 from flask_mongoengine import MongoEngine
+import mongoengine as me
+# from marshmallow_mongoengine import ModelSchema
+import datetime
 
 app = Flask(__name__)
 api = Api(app)
 
-# #Data base initialization
-# app.config['MONGODB_SETTINGS'] = {
-#     'db': 'recruiter_database',
-#     'host': 'localhost',
-#     'port': 27017
-# }
+# Data base initialization
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'recruiter_database',
+    'host': 'localhost',
+    'port': 27017
+}
+
+db = MongoEngine()
+db.init_app(app)
+me.connect('recruiter_database')
+
+class CV(db.Document):
+    id = db.IntField(primary_key=True, default=1)
+    name = db.StringField()
+
+class Job(db.Document):
+    id = db.IntField(primary_key=True, default=1)
+    title = db.StringField()
+    # description = me.StringField()
+    # location = me.StringField()
+    # is_open = me.BooleanField()
+    # last_time_opened = me.DateTimeField(default=datetime.datetime.utcnow)
+    #
+    # def __repr__(self):
+    #     return '<Job(title = {self.title!r})>'.format(self=self)
+
+
+# class JobSchema(ModelSchema):
+#     class Meta:
+#         model = Job
+
+
+# job_schema = JobSchema()
+
+# first_job = Job(id=123456,
+#                 title='System analyzer',
+                # description='System analyzer in a global company',
+                # location='Tel-Aviv',
+                # is_open=True).save()
+
+first_job = Job(id=123456, title='System analyzer').save()
+second_job = Job(id=123457, title='Python developer').save()
+cv1 = CV(id =368, name='Sivan Itescu').save()
+# print(first_job)
+# dump_data = job_schema.dump(first_job).data
 #
-# db = MongoEngine()
-# db.init_app(app)
-#
-# class User(db.Document):
-#     name = db.StringField()
-#     email = db.StringField()
+# job_schema.load(dump_data).data
 
-jobs = {}
+# for job in Job.objects:
+#     print(job.title)
+# print(Job.objects)
+# print(type(Job.objects))
+# jobs = {}
 
 
-class Jobs(Resource):
+class JobsResource(Resource):
     def get(self):
-        return jobs
+        result = {}
+        for job in Job.objects:
+            # print(j.title)
+            result[job.id] = job.title
+        return result
 
-class Job(Resource):
-    def get(self, job_id):
-        return {job_id: jobs[job_id]}
+class JobResource(Resource):
+    # def get(self, job_id):
+    #     return {job_id: jobs[job_id]}
 
-    def post(self,job_id):
-        jobs[job_id] = request.form['job_title']
-        return {job_id: jobs[job_id]}, 201
+    def put(self,job_id):
+        put_job = Job(id=job_id, title=request.form['job_title']).save()
+        return {job_id: request.form['job_title']}, 201
 
 
-api.add_resource(Jobs, '/alljobs')
-api.add_resource(Job, '/jobs/<string:job_id>')
+api.add_resource(JobsResource, '/alljobs')
+api.add_resource(JobResource, '/jobs/<string:job_id>')
 
 @app.route('/about')
 def index():
--- RecruitEmployeeSystem/client_test.py
+++ RecruitEmployeeSystem/client_test.py
@@ -5,16 +5,16 @@ BASE = "http://127.0.0.1:5000/"
 # response = requests.get(BASE)
 # print(response.json())
 
-response = requests.post(BASE+'/jobs/1', data={'job_title': 'Senior SW Engineer at Goggle'})
+response = requests.put(BASE+'/jobs/1', data={'job_title': 'Senior SW Engineer at Goggle'})
 print(response.json())
 
-response = requests.post(BASE+'/jobs/2', data={'job_title': 'Junior SW Engineer at Goggle'})
+response = requests.put(BASE+'/jobs/2', data={'job_title': 'Junior SW Engineer at Goggle'})
 print(response.json())
 
-response = requests.post(BASE+'/jobs/3', data={'job_title': 'Senior SW Engineer at Amazon'})
+response = requests.put(BASE+'/jobs/3', data={'job_title': 'Senior SW Engineer at Amazon'})
 print(response.json())
 
-response = requests.post(BASE+'/jobs/4', data={'job_title': 'Senior SW Engineer at Apple'})
+response = requests.put(BASE+'/jobs/4', data={'job_title': 'Senior SW Engineer at Apple'})
 print(response.json())
 
 # response = requests.get(BASE+'/jobs/1')
